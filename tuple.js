235ba1951c44b3b89b7cfb943c5dda3ddac0479d
Rishikesh K V <rishikeshkaroth@gmail.com>
2024-07-12 02:27:34 +0530

    tuples in ts

---
+++ tuple.js
@@ -0,0 +1,9 @@
+"use strict";
+// tuple - have the array with types in a specific order
+Object.defineProperty(exports, "__esModule", { value: true });
+var tup;
+tup = ["hi", 1, true];
+var rgb = [255, 1, 234];
+var newUser = [1, "Rishi"];
+//values can be changed
+newUser[1] = "rishi";
---
+++ tuple.ts
@@ -0,0 +1,18 @@
+// tuple - have the array with types in a specific order
+
+let tup : [string ,number , boolean ]
+
+tup = ["hi" , 1 ,true]
+
+
+let rgb: [number , number , number] = [255,1,234]
+
+type User = [number , string]
+
+const newUser : User = [1, "Rishi"]
+
+//values can be changed
+newUser[1] = "rishi"
+
+
+export {}
